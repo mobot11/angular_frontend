fd05afe57bfc8f601f691d836ace4a5e06985cee
Dylan T. Moberg <dylanmoberg@msn.com>
2015-08-04 18:27:23 -0700

    not sure what i did

--- app/js/client.js
+++ app/js/client.js
@@ -1,3 +1,10 @@
 require('angular/angular');
 
 var playerApp = angular.module('playerApp', []);
+
+var playerController = playerApp.controller('playerController', ['$scope', function($scope) {
+	$scope.greeting = 'welcome to my NBA players App!'
+	$scope.alertGreeting = function() {
+    
+	}
+}])
--- build/bundle.js
+++ build/bundle.js
@@ -48,6 +48,13 @@
 
 	var playerApp = angular.module('playerApp', []);
 
+	var playerController = playerApp.controller('playerController', ['$scope', function($scope) {
+		$scope.greeting = 'welcome to my NBA players App!'
+		$scope.alertGreeting = function() {
+	    
+		}
+	}])
+
 
 /***/ },
 /* 1 */
